736bb5718e12de70c059102a306d14d85def3d15
Daze <my873781203@gmail.com>
2019-08-05 19:35:52 +0800

    postcss配置

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div class="test"></div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,5 +22,11 @@
       color: #42b983;
     }
   }
+}
+.test {
+  width: 500px;
+  height: 500px;
+  background-color: red;
+  margin: 100px auto 0;
 }
 </style>
